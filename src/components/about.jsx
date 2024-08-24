d764af623ad899a28612b1e0eecbaeef7be4814e
thibault livran <thibault.livran@dnd.fr>
2024-08-24 18:00:53 +0200

    integration: update about component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,40 +1,68 @@
 import React from "react";
-
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.compat.css"
 const About = () => (
     <div className="about content-wrapper" id="about">
-        <h2 className="part-title hidden">A propos</h2>
+        <ScrollAnimation animateIn='fadeIn' animateOnce={true}>
+            <h2 className="part-title hidden">A propos</h2>
+        </ScrollAnimation>
         <div className="about__container">
             <div className="about__content">
                 <div className="about__content__image hidden">
-                    <img src="images/TIBO-white.svg" alt="" />
+                    <ScrollAnimation animateIn='fadeIn' delay={250} animateOnce={true}>
+                            <img src="images/moi/TIBO.png" alt="" />
+                    </ScrollAnimation>
                 </div>
-
                 <div className="about__content__txt hidden">
-                    <p>Depuis tout petit, j’ai toujours été attiré par l’art. Je passais des heures à dessiner, à peaufiner chaque détail, et je n’étais jamais aussi heureux qu’avec un crayon à la main. Au lycée, en plus de mes cours d'art plastique, j’ai découvert l’informatique, un domaine qui m’a captivé tout autant. J’ai rapidement compris que cette discipline offrait un potentiel créatif immense, mais d’une manière différente.</p>
+                    <ScrollAnimation animateIn='fadeIn' animateOnce={true}>
+                        <p>Depuis tout petit, <span class="violet-text">j’ai toujours été attiré par l’art</span>. Je passais des heures à <span class="violet-text">dessiner</span>, à <span class="violet-text">peaufiner chaque détail</span>, et je n’étais jamais aussi heureux qu’avec <span class="violet-text">un crayon à la main</span>. <span class="violet-text">Au lycée</span>, en plus de mes cours d'<span class="violet-text">art plastique</span>, j’ai découvert <span class="violet-text">l’informatique</span>, un domaine qui m’a captivé tout autant. J’ai rapidement compris que cette discipline offrait un potentiel créatif immense, mais d’une manière différente.</p>
+                            {/* <p>Depuis tout petit, <span className="violet-text">j’ai toujours été attiré par l’art</span>. Je passais des heures à <span className="violet-text">dessiner</span>, à <span className="violet-text">peaufiner chaque détail</span>, et je me sentais heureux avec un crayon à la main. Au lycée, en plus de mes cours <span className="violet-text">d'art plastique</span>, j’ai découvert <span className="violet-text">l’informatique</span>, un domaine qui m’a captivé tout autant. J’ai rapidement compris que cette discipline offrait un potentiel créatif immense, mais d’une manière différente.</p> */}
+                    </ScrollAnimation>
+                </div>
+            </div>
+            <div className="about__content reverse">
+                <div className="about__content__txt hidden">
+                    <ScrollAnimation animateIn='fadeIn' animateOnce={true}>
+                        {/* <p>
+                            À ce moment-là, j’ai commencé à me poser une question : comment pouvais-je lier ces deux passions ? <span class="violet-text">Comment combiner l’art</span>, qui me permettait de créer des formes et des émotions, <span class="violet-text">avec l’informatique</span>, qui offrait un terrain de jeu infini pour l’innovation et la technique ? <span class="violet-text">Le développement web est rapidement apparu comme une évidence</span>, un domaine où je pouvais fusionner ces deux mondes qui me tenaient à cœur.
+                        </p> */}
+                        <p>
+                            À ce moment-là, je me suis demandé comment lier ces deux passions : comment unir la <span class="violet-text">créativité</span> de l’art avec le potentiel de <span class="violet-text">l’informatique</span> ? Le <span class="violet-text">développement web</span> est vite devenu une évidence, me permettant de canaliser ma créativité pour résoudre des problèmes et créer des <span class="violet-text">solutions innovantes</span>. Cela m'a permis de fusionner ces deux aspects de ma personnalité qui me tiennent à cœur.
+                        </p>
+                    </ScrollAnimation>
+                </div>
+                <div className="about__content__image hidden">
+                    <ScrollAnimation animateIn='fadeIn' delay={250} animateOnce={true}>
+                            <img src="images/moi/TIBO_DESSIN.png" alt="" />
+                    </ScrollAnimation>
                 </div>
             </div>
             <div className="about__content">
-                <div className="about__content__txt hidden">
-                    <p>À ce moment-là, j’ai commencé à me poser une question : comment pouvais-je lier ces deux passions ? Comment combiner l’art, qui me permettait de créer des formes et des émotions, avec l’informatique, qui offrait un terrain de jeu infini pour l’innovation et la technique ? Le développement web est rapidement apparu comme une évidence, un domaine où je pouvais enfin fusionner ces deux mondes qui me tenaient à cœur.</p>
-                </div>
                 <div className="about__content__image hidden">
-                    <img src="images/PC-TIBO-white.svg" alt="" />
+                    <ScrollAnimation animateIn='fadeIn' delay={250} animateOnce={true} animateOnce={true}>
+                            <img src="images/moi/TIBO_DND.png" alt="" />
+                    </ScrollAnimation>
+                </div>
+                <div className="about__content__txt hidden">
+                    <ScrollAnimation animateIn='fadeIn' animateOnce={true}>
+                        <p>
+                            L’immersion chez <span class="violet-text">Dn’D</span> pendant <span class="violet-text">deux ans</span> en alternance a été une <span class="violet-text">opportunité incroyable</span> pour approfondir ma passion pour le <span class="violet-text">développement</span>. Travailler sur des <span class="violet-text">projets concrets</span>, entouré de personnes aussi <span class="violet-text">passionnées</span> que moi, m’a permis de comprendre comment la <span class="violet-text">créativité</span> et la <span class="violet-text">technologie</span> peuvent s’entrelacer pour créer des <span class="violet-text">solutions innovantes</span>. Ces <span class="violet-text">deux années</span> ont été un vrai plaisir, et elles n’ont fait que renforcer mon envie de continuer à explorer ce domaine.
+                        </p>
+                    </ScrollAnimation>
                 </div>
             </div>
-            <div className="about__content">
-                <div className="about__content__image hidden">
-                    <img src="images/TIBO-white.svg" alt="" />
-                </div>
+            <div className="about__content reverse">
                 <div className="about__content__txt hidden">
-                    <p>L’immersion chez Dn’D pendant deux ans en alternance a été une chance incroyable pour approfondir cette double passion. Travailler sur des projets concrets, entouré de personnes aussi passionnées que moi, m’a permis de découvrir comment l’art et la technologie peuvent s’entrelacer pour créer des expériences uniques. Ces deux années ont été un vrai plaisir, et elles n’ont fait que renforcer mon envie de continuer sur cette voie.</p>
-                </div>
-            </div>
-            <div className="about__content">
-                <div className="about__content__txt hidden">
-                    <p>Aujourd’hui, je suis plus motivé que jamais à poursuivre dans le développement, à explorer de nouvelles façons de marier l’esthétique et la technologie, et à continuer à créer des projets qui me permettent de m’épanouir pleinement dans ces deux domaines qui me passionnent profondément.</p>
+                    <ScrollAnimation animateIn='fadeIn' animateOnce={true}>
+                        <p>
+                            Aujourd’hui, je suis <span class="violet-text">plus motivé que jamais</span> à poursuivre dans le <span class="violet-text">développement</span>, à explorer de nouvelles façons de marier <span class="violet-text">l’esthétique</span> et la <span class="violet-text">technologie</span>, et à continuer à créer des <span class="violet-text">projets</span> qui me permettent de m’épanouir pleinement dans ces deux <span class="violet-text">domaines</span> qui me <span class="violet-text">passionnent profondément</span>.
+                        </p>
+                    </ScrollAnimation>
                 </div>
                 <div className="about__content__image hidden">
-                    <img src="images/TIBO-white.svg" alt="" />
+                    <ScrollAnimation animateIn='fadeIn' delay={250} animateOnce={true}>
+                        <img src="images/moi/TIBO_ORDI.png" alt="" />
+                    </ScrollAnimation>
                 </div>
             </div>
         </div>
