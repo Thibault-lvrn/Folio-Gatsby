31fd5215560480ee31495bb736c0baa273e62781
thibault livran <thibault.livran@dnd.fr>
2024-08-24 17:59:57 +0200

    integration: Update footer styles and add contact buttons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,6 +8,14 @@ const Footer: React.FC = () => (
                     <p>© 2024 Thibault Livran</p>
                 </div>
                 <div className="footer__content__left">
+                    <div className="footer__content__left__btns">
+                        <button className="btn btn-contact" onClick={() => window.location.href = "mailto:thibault.livran@gmail.com"}>
+                            Mon CV
+                        </button>
+                        <button className="btn btn-contact" onClick={() => window.location.href = "mailto:thibault.livran@gmail.com"}>
+                            Contactez moi
+                        </button>
+                    </div>
                     <a href="https://github.com/Thibault-lvrn" target="_blank">
                         <img loading="lazy" src="images/github.png" alt="github" />
                     </a>
